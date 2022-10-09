f429e62e00d4fffdbbf560918a7863bc2226aa70
Masasi Arai <masasi.arai@gmail.com>
2022-10-09 16:51:17 +0800

    Initial Commit

---
+++ App.js
@@ -0,0 +1,22 @@
+import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import SplashScreen from './Components/SplashScreen';
+
+export default function App() {
+  return (
+    <SafeAreaProvider>
+      <SplashScreen>
+        
+      </SplashScreen>
+    </SafeAreaProvider>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
